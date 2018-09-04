f8e04b1d9585e7579be6c899497b4b3f0753814d
Thiago Giammattey <giammattey@gmail.com>
2018-09-04 10:42:39 -0300

    added basic UI

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import PostForm from "./PostForm";
+import AllPosts from "./AllPosts";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <PostForm />
+        <AllPosts />
+      </div>
+    );
+  }
+}
+
+export default App;
