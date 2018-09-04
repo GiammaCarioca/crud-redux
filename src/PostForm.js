ef3fa3050b8025b0a808dd9ed4778a5afd68710c
Thiago Giammattey <giammattey@gmail.com>
2018-09-04 12:24:54 -0300

    handling the onSubmit event of the form

--- src/PostForm.js
+++ src/PostForm.js
@@ -1,15 +1,38 @@
 import React, { Component } from "react";
 
 class PostForm extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    const title = this.getTitle.value;
+    const message = this.getMessage.value;
+    const data = {
+      id: new Date(),
+      title,
+      message
+    };
+    // This is to make sure that the data is being captured
+    console.log(data);
+  };
   render() {
     return (
       <div>
         <h1>Create Post</h1>
-        <form>
-          <input required type="text" placeholder="Enter Post Title" />
+        <form onSubmit={this.handleSubmit}>
+          <input
+            required
+            type="text"
+            ref={input => (this.getTitle = input)}
+            placeholder="Enter Post Title"
+          />
           <br />
           <br />
-          <textarea required rows="5" cols="28" placeholder="Enter Post" />
+          <textarea
+            required
+            rows="5"
+            ref={input => (this.getMessage = input)}
+            cols="28"
+            placeholder="Enter Post"
+          />
           <br />
           <br />
           <button>Post</button>
@@ -18,5 +41,4 @@ class PostForm extends Component {
     );
   }
 }
-
 export default PostForm;
